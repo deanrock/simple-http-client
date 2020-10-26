5c7f3d810e051184e246ab54d2d93a5d0aa65eb0
Dejan Levec <dejan@celtra.com>
2020-10-26 18:31:13 +0100

    solve host beforehand

--- main.php
+++ main.php
@@ -6,7 +6,29 @@ use GuzzleHttp\Client;
 
 require_once 'vendor/autoload.php';
 
-function makeRequest(int $iteration)
+function unparse_url(array $parsed): string {
+    $pass      = $parsed['pass'] ?? null;
+    $user      = $parsed['user'] ?? null;
+    $userinfo  = $pass !== null ? "$user:$pass" : $user;
+    $port      = $parsed['port'] ?? 0;
+    $scheme    = $parsed['scheme'] ?? "";
+    $query     = $parsed['query'] ?? "";
+    $fragment  = $parsed['fragment'] ?? "";
+    $authority = (
+        ($userinfo !== null ? "$userinfo@" : "") .
+        ($parsed['host'] ?? "") .
+        ($port ? ":$port" : "")
+    );
+    return (
+        (\strlen($scheme) > 0 ? "$scheme:" : "") .
+        (\strlen($authority) > 0 ? "//$authority" : "") .
+        ($parsed['path'] ?? "") .
+        (\strlen($query) > 0 ? "?$query" : "") .
+        (\strlen($fragment) > 0 ? "#$fragment" : "")
+    );
+}
+
+function makeRequest(int $iteration, $url)
 {
     $client = new Client();
     $timeout = 5;
@@ -14,7 +36,7 @@ function makeRequest(int $iteration)
     $e = explode("-", getenv('HOSTNAME'));
     $hostname = end($e);
 
-    $res = $client->request('GET', getenv('URL') . "?iter={$iteration}-{$hostname}", [
+    $res = $client->request('GET', $url . "?iter={$iteration}-{$hostname}", [
         'headers' => [],
         'connect_timeout' => $timeout,
         'read_timeout' => $timeout,
@@ -56,14 +78,23 @@ function shutdown()
 
 pcntl_signal(SIGINT,"shutdown");
 
+$parsed_url = parse_url(getenv('URL'));
+$endpoints = gethostbynamel($parsed_url['host']);
+
+$urlWithRandomEndpoint = function() use ($parsed_url, $endpoints): string {
+    $parsed_url['host'] = $endpoints[array_rand($endpoints)];
+
+    return unparse_url($parsed_url);
+};
+
 $iteration = 0;
 
 while (true) {
     $iteration++;
     
-    timeExecution($iteration, function() use ($iteration) {
-        handleException(function() use ($iteration) {
-            makeRequest($iteration);
+    timeExecution($iteration, function() use ($iteration, $urlWithRandomEndpoint) {
+        handleException(function() use ($iteration, $urlWithRandomEndpoint) {
+            makeRequest($iteration, $urlWithRandomEndpoint());
         });
     });
 }
